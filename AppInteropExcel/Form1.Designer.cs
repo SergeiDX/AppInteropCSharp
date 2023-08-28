89325cbca91d06dcdb435b94ade5996867d50e5d
alber <alber@DESKTOP-EUB75BK>
2023-08-27 23:13:15 -0600

    Add project files.

---
+++ AppInteropExcel/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace AppInteropExcel
+{ 
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txttexto = new System.Windows.Forms.TextBox();
+            this.btndocx = new System.Windows.Forms.Button();
+            this.btnxls = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txttexto
+            // 
+            this.txttexto.Location = new System.Drawing.Point(9, 59);
+            this.txttexto.Name = "txttexto";
+            this.txttexto.Size = new System.Drawing.Size(336, 20);
+            this.txttexto.TabIndex = 1;
+            // 
+            // btndocx
+            // 
+            this.btndocx.BackgroundImage = global::AppInteropExcel.Properties.Resources.xls;
+            this.btndocx.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btndocx.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btndocx.Location = new System.Drawing.Point(199, 115);
+            this.btndocx.Name = "btndocx";
+            this.btndocx.Size = new System.Drawing.Size(74, 62);
+            this.btndocx.TabIndex = 2;
+            this.btndocx.UseVisualStyleBackColor = true;
+            this.btndocx.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnxls
+            // 
+            this.btnxls.BackColor = System.Drawing.Color.Transparent;
+            this.btnxls.BackgroundImage = global::AppInteropExcel.Properties.Resources.doc;
+            this.btnxls.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnxls.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnxls.Location = new System.Drawing.Point(80, 115);
+            this.btnxls.Name = "btnxls";
+            this.btnxls.Size = new System.Drawing.Size(74, 62);
+            this.btnxls.TabIndex = 0;
+            this.btnxls.UseVisualStyleBackColor = false;
+            this.btnxls.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(76, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 19);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Aplicacion Interop Word/Excel";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(357, 222);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btndocx);
+            this.Controls.Add(this.txttexto);
+            this.Controls.Add(this.btnxls);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnxls;
+        private System.Windows.Forms.TextBox txttexto;
+        private System.Windows.Forms.Button btndocx;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ AppInteropExcel/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Word = Microsoft.Office.Interop.Word;
+using Excel = Microsoft.Office.Interop.Excel;
+namespace AppInteropExcel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dialogoex = new SaveFileDialog();
+
+            if (dialogoex.ShowDialog() == DialogResult.OK)
+            {
+                object obj = System.Reflection.Missing.Value;
+                Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
+                Microsoft.Office.Interop.Excel.Workbook file = excel.Workbooks.Add(obj);
+                Microsoft.Office.Interop.Excel.Worksheet hoja = file.Worksheets.Add();
+                file.Activate();
+                hoja.Cells[1, 1] = txttexto.Text;
+                file.SaveAs(dialogoex.FileName);
+                excel.Visible = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dialogo = new SaveFileDialog();
+            if (dialogo.ShowDialog() != DialogResult.OK)
+            { return; }
+            string ruta = dialogo.FileName;
+            var wordApp = new Word.Application();
+            wordApp.Visible = true;
+            wordApp.Documents.Add();
+            string dato = txttexto.Text;
+            wordApp.Selection.TypeText(dato);
+            wordApp.ActiveDocument.SaveAs2(ruta);
+        }
+    }
+}
